dbff7474cc2a3293370459cf95c8ac4957307e40
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-11-05 16:12:47 +0200

    init

---
+++ RoosterAndFriends/App/RoosterAndFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoosterAndFriendsApp.swift
+//  RoosterAndFriends
+//
+//  Created by Serhii Babchuk on 05.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct RoosterAndFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RALaunchView()
+        }
+    }
+}
